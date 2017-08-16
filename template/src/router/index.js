3485c8f96f8b8f0d1372b0a6ce5b8a423a10f731
cuijj <jiajie.cui@midea.com.cn>
2017-08-16 10:55:12 +0800

    修改注释

--- template/src/router/index.js
+++ template/src/router/index.js
@@ -2,7 +2,7 @@ const demo = resolve => require(['../components/demo'], resolve);
 
 let routes = [
     {
-        // TODO  该路由仅用于示例--k，与项目无关，请自行删除
+        // TODO  该路由仅用于示例，与项目无关，请自行删除
         path: '/',
         component: demo,
     }
