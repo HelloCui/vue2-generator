fea61fefa8922231539df0ccea910580865b9d00
徐建辉 <xujianhui@xujianhuideMacBook-Pro.local>
2018-02-08 13:46:22 +0800

    增加babel-polyfill，修改静态资源打包配置

--- template/build/utils.js
+++ template/build/utils.js
@@ -30,7 +30,8 @@ exports.cssLoaders = function(options) {
         if (options.extract) {
             return ExtractTextPlugin.extract({
                 use: sourceLoader,
-                fallback: 'vue-style-loader'
+                fallback: 'vue-style-loader',
+                publicPath:"../../"
             })
         } else {
             return ['vue-style-loader', sourceLoader].join('!')
